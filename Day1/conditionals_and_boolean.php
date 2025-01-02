1026818f9ab87f963094ae12ae75f326bb020cb8
sajo13 <sajosunny007@gmail.com>
2025-01-02 15:24:48 +0530

    Define Boolean value to check the conditional
    Add examples for conditional check using if-else statement

---
+++ Day1/conditionals_and_boolean.php
@@ -0,0 +1,16 @@
+<?php
+
+/* ----- Conditionals & Boolean ----- */
+
+
+$name = "Marcos";
+
+$read = true;
+
+if($read) {
+ $message = "You have read ". " " . $name;
+} else {
+ $message = "You haven't read". " ". $name;
+}
+
+echo($message);
