587ef96d5feb3986d559b1386fdfe7faceb46e36
sajo13 <sajosunny007@gmail.com>
2025-01-03 12:47:08 +0530

    Implement string manipulation functions
    Find length, position, reverse, and convert to uppercase or lowercase

---
+++ Day2/string_fuctions.php
@@ -0,0 +1,26 @@
+<?php
+/* ---------- String Functions -------- */
+
+/*
+  Functions to work with strings
+*/
+
+$string = 'Hello World';
+
+// Get the length of a string
+echo strlen($string);
+
+// Find the position of the first occurrence of a substring in a string
+echo strpos($string, 'o');
+
+// Find the position of the last occurrence of a substring in a string
+echo strrpos($string, 'o');
+
+// Reverse a string
+echo strrev($string);
+
+// Convert all characters to lowercase
+echo strtolower($string);
+
+// Convert all characters to uppercase
+echo strtoupper($string);
