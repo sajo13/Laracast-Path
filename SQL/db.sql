f7e8f3068d204093c88502e6ab7aa52d82a9e245
sajo13 <sajosunny007@gmail.com>
2025-01-06 16:50:09 +0530

    Create new database and posts table.

---
+++ SQL/db.sql
@@ -0,0 +1,13 @@
+
+-- post table create ----
+CREATE DATABASE IF NOT EXISTS LARACAST;
+
+USE LARACAST;
+
+CREATE TABLE posts (
+    post_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+);
+
+
+INSERT INTO `posts` (`post_id`, `title`) VALUES ('1', 'TEST'), ('2', 'TEST2');
