a07d585d78e3ce14b3c31a4aead026b4cb9dcbed
sajo13 <sajosunny007@gmail.com>
2025-01-06 17:20:27 +0530

    Connect to mysql database and fetch all posts data.

--- Day3/index.php
+++ Day3/index.php
@@ -1,6 +1,22 @@
 <?php
 require "functions.php";
 
-require "routes.php";
+// require "routes.php";
 
+//connect to our MySql db
+$dsn = "mysql:host=localhost;port=3306;dbname=laracast";
+$username = "root";
+$password = "";
+
+$pdo = new PDO($dsn, $username, $password);
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ( $posts as $post) {
+    echo "<li>". $post['title'] ."</li>";
+}
 ?>
