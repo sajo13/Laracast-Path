e53a888daaf8240ba39caf5e67544cc0e1659e48
sajo13 <sajosunny007@gmail.com>
2025-01-07 12:12:31 +0530

    Add users and notes table

--- SQL/db.sql
+++ SQL/db.sql
@@ -15,3 +15,38 @@ INSERT INTO `posts` (`post_id`, `title`) VALUES ('1', 'TEST'), ('2', 'TEST2');
 
 -- show all posts ---
 Select * from posts;
+
+
+CREATE TABLE `users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(255) NOT NULL,
+  `admin` TINYINT(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+);
+
+
+drop table users;
+
+-- create notes table ---
+
+CREATE TABLE `notes` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `body` text NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `unique_email` (`email`)
+);
+
+ALTER TABLE `notes`
+ADD `user_id` INT(11) NOT NULL DEFAULT '1',
+ADD CONSTRAINT `fk_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users`(`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE;
