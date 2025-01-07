f2cc364a8619d68e7e0aab715f08e414ad89ae35
sajo13 <sajosunny007@gmail.com>
2025-01-07 09:58:39 +0530

    Hardcoded database connection moved to seperate config file

--- Day4/database.php
+++ Day4/database.php
@@ -4,13 +4,14 @@ class Database
 {
     public $connection;
 
-    public function __construct()
+    public function __construct($config, $username = 'root', $password  ='')
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=laracast";
-        $username = "root";
-        $password = "";
         
-        $this->connection = new PDO($dsn, $username, $password);
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+        
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
     public function query($query)
--- Day4/index.php
+++ Day4/index.php
@@ -5,9 +5,9 @@ require "functions.php";
 require "database.php";
 
 //connect to our database, and execute a query
-
-$db = new Database();
-$posts = $db->query("select * from posts");
+$config = require('config.php');
+$db = new Database($config);
+$posts = $db->query("select * from posts")->fetchAll();
 
 
 foreach ( $posts as $post) {
