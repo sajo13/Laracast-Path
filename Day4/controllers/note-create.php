c077124919fa61f4b720aadfa051610c27cfccdc
sajo13 <sajosunny007@gmail.com>
2025-01-07 18:01:53 +0530

    Basic Validator class for text input

--- Day4/controllers/note-create.php
+++ Day4/controllers/note-create.php
@@ -1,4 +1,6 @@
 <?php
+require 'Validator.php';
+
 $config = require('config.php');
 $db = new Database($config);
 
@@ -8,8 +10,10 @@ $heading = 'Create Note';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $errors = [];
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = 'A body is required';
+
+
+    if(! $Validator::string($_POST['body'], 1 , 1000)) {
+        $errors['body'] = 'A body of no more than 1000  chars required';
     }
 
 
