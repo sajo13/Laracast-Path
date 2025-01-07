a18434dd49b08b64c27829ae3611cf91d71f7503
sajo13 <sajosunny007@gmail.com>
2025-01-07 17:21:53 +0530

    Add client and server validation to Note create page

--- Day4/controllers/note-create.php
+++ Day4/controllers/note-create.php
@@ -5,10 +5,23 @@ $db = new Database($config);
 
 $heading = 'Create Note';
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)',[
-        'body' => $_POST['body'],
-        'user_id' => 4  
-]);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $errors = [];
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'A body can not be more than 1000';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 4
+        ]);
+    }
 }
 require 'views/notes-create.view.php';
--- Day4/views/notes-create.view.php
+++ Day4/views/notes-create.view.php
@@ -20,7 +20,16 @@
           <div class="col-span-full">
             <label for="body" class="block text-sm/6 font-medium text-gray-900">Description</label>
             <div class="mt-2">
-              <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+              <textarea name="body" id="body" required rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                <?= $_POST['body'] ?? '' ?>
+              </textarea>
+            
+            <?php if (isset($errors['body'])) : ?>
+              <?php echo $errors['body']; ?>
+
+            <?php endif; ?>
+            
+            
             </div>
           </div>
           <p>
