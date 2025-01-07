3e501716c93698aab408f6901efe475760ecb44e
sajo13 <sajosunny007@gmail.com>
2025-01-07 10:38:36 +0530

    To avoid SQL injection use parameterized queries

--- Day4/index.php
+++ Day4/index.php
@@ -7,7 +7,10 @@ require "database.php";
 //connect to our database, and execute a query
 $config = require('config.php');
 $db = new Database($config);
-$posts = $db->query("select * from posts")->fetchAll();
+$id = $_GET['id'];
+
+$query = "select * from posts where id = ?";
+$posts = $db->query($query, [$id])->fetchAll();
 
 
 foreach ( $posts as $post) {
