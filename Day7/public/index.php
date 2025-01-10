bcfa35d6353490bcca39b3fa7d33971d627dcd29
sajo13 <sajosunny007@gmail.com>
2025-01-10 15:11:12 +0530

    Introduce Composer for dependency management and autoloading
    - Set up Composer to manage project dependencies and packages.
    - Implement Composer's autoloader for efficient class loading.
    - Simplify package installation and management with Composer commands.

--- Day7/public/index.php
+++ Day7/public/index.php
@@ -5,16 +5,12 @@ use Core\ValidationException;
 session_start();
 
 const BASE_PATH = __DIR__.'/../';
+require BASE_PATH. 'vendor/autoload.php';
+
+
+
 require BASE_PATH.'Core/functions.php';
 
-
-// require base_path("database.php");
-// require base_path("Response.php");
-spl_autoload_register(function ($class) {
-
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
 require BASE_PATH. 'bootstrap.php';
 
 $router = new \Core\Router(); 
